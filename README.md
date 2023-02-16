d04ac27986045eeea9663648226fe6c18162fbe6
DiFFtY <diffty@gmail.com>
2023-02-16 17:12:39 +0100

    Initial commit (starting from ts-threejs-boilerplate)

---
+++ README.md
@@ -0,0 +1,2 @@
+Le Musehum (aka. Project huM)
+=====
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { GLTFSample } from "./samples/gltf-sample"
+
+const app = new GLTFSample();
+document.body.appendChild(app.renderer.domElement);
