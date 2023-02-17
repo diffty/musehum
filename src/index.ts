e91d49b10530b90e4bc46fd713fbc62ab3751070
DiFFtY <diffty@gmail.com>
2023-02-17 02:25:43 +0100

    test link to interact with prev/next artefact

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { MusehumApp } from "./musehum-app"
 const app = new MusehumApp();
 document.body.appendChild(app.renderer.domElement);
 
-function test() {
-    console.log("test");
+let nextlink = document.getElementById("next-link");
+if (nextlink) {
+    nextlink.addEventListener("click", (e: Event) => { app.viewer.nextArtefact(); });
+}
+
+let prevlink = document.getElementById("prev-link");
+if (prevlink) {
+    prevlink.addEventListener("click", (e: Event) => { app.viewer.prevArtefact(); });
 }
