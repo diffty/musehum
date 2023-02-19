5ffe6e339a0b7c0822c693116bde6d5f9254c133
DiFFtY <diffty@gmail.com>
2023-02-19 12:58:34 +0100

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { MusehumApp } from "./musehum-app"
 const app = new MusehumApp();
 document.body.appendChild(app.renderer.domElement);
 
-let nextlink = document.getElementById("next-link");
+const nextlink = document.getElementById("next-link");
 if (nextlink) {
     nextlink.addEventListener("click", (e: Event) => { app.viewer.nextArtefact(); });
 }
 
-let prevlink = document.getElementById("prev-link");
+const prevlink = document.getElementById("prev-link");
 if (prevlink) {
     prevlink.addEventListener("click", (e: Event) => { app.viewer.prevArtefact(); });
 }
