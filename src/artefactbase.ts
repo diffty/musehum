e7d054476d8b2b311d8ca5503e00110eb8452106
DiFFtY <diffty@gmail.com>
2023-06-25 19:16:57 +0200

    final assets dump, html/css ui animations

--- src/artefactbase.ts
+++ src/artefactbase.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TextureLoader } from "three";
 
 import { ArtefactData } from './artefactdata'
 
@@ -20,12 +21,24 @@ export class ArtefactBase extends THREE.Group {
 
     load(onLoadingEndCallback?: (artefact: ArtefactBase) => void) {
         const gltfLoader = new GLTFLoader();
+        const texLoader = new TextureLoader();
 
         gltfLoader.load(
             this.data.fileName,
             (gltf) => {
                 this.add(gltf.scene);
                 this.obj = gltf.scene;
+                if (this.data.fileName == "peggy.gltf") {
+                    gltf.scene.traverse((o: THREE.Object3D) => {
+                        if (o.type == "Mesh") {
+                            //o.material.transparent = true;
+                            //o.material.map = texLoader.load("captex.png");
+                            //o.material.alphaMap = texLoader.load("captex_alpha.png");
+                            o.material.side = THREE.DoubleSide;
+                            //o.material.alphaTest = 1;
+                        }
+                    })
+                }
                 if (onLoadingEndCallback) {
                     onLoadingEndCallback(this);
                     this.updateStencilId();
